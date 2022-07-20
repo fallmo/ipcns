36651231c2edfc97f2ccab8eb3d7e75a019c45ee
Mohamed Fall <ps.hackmaster@gmail.com>
2022-07-20 09:29:44 +0000

    msg bug

--- handle_update.sh
+++ handle_update.sh
@@ -1,7 +1,7 @@
 source ./vars.conf
 
 function send_mail {
-    message = $(printf "Subject: IP Update\n\nCurrent IP: $new_ip\nDate: $(date +%c)");
+    message=$(printf "Subject: IP Update\n\nCurrent IP: $new_ip\nDate: $(date +%c)");
     echo $message | msmtp -a default --from=$smtp_email --user=$smtp_email --passwordeval="echo $smtp_password" --logfile=./mail.log $destination
     echo "mail sent to: $destination"
 }
