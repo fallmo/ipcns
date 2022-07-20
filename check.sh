76b2fc8476729b95182787d3c899b72904ba8858
Mo <ps.hackmaster@gmail.com>
2022-07-20 09:23:38 +0000

    stop on fail

--- check.sh
+++ check.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 
-new_ip=$(curl -s ipecho.net/plain);
+new_ip=$(curl -s ipecho.net/plain && echo || echo "failed");
 old_ip=$(cat ./ip 2> /dev/null);
 
+if [ "$new_ip" == "failed" ]; then echo "failed to fetch"; exit 1; fi;
 if [ "$new_ip" == "$old_ip" ]; then echo "no change."; exit 0; fi;
 
 echo "ip change."
